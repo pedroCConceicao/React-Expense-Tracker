14b9086f6e06c394e681e55db6cd55db3f58904f
Pedro <pedrocconce@gmail.com>
2024-03-27 17:21:44 -0300

    Creating the controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,16 @@
-// get categories
+const model = require('../models/model.js')
+
+// post: http://localhost:8080/api/categories
 function create_Categories(req, res) {
-    res.json("Get Request from Categories");
+    const Create = new model.Categories({
+        type: 'Savings',
+        color: '#1F3B5C',
+    })
+
+    Create.save(function(error){
+        if(!error) return res.json(Create);
+        return res.status(400).json({ message: `Erro ao criar categorias ${error}` });
+    })
 }
 
 module.exports = {
